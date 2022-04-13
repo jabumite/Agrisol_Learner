f497b9d6798c56606cbddf1531b068655c3ee708
Jabumite <69894128+jabumite@users.noreply.github.com>
2022-04-13 04:44:47 +0200

    nuwe agrisol program

---
+++ src/app/components/pricing/pricing.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { RoomModel } from 'src/app/models/roomModel';
+import { CartService } from 'src/app/services/cart.service';
+import { RoomService } from 'src/app/services/room.service';
+
+@Component({
+  selector: 'app-pricing',
+  templateUrl: './pricing.component.html',
+  styleUrls: ['./pricing.component.sass']
+})
+export class PricingComponent implements OnInit {
+
+  rooms: RoomModel[] = [];
+
+  selectedRoom :RoomModel | undefined;
+
+  constructor(private roomService: RoomService, private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.rooms = this.roomService.availableRooms;
+  }
+
+  selectClicked(room: RoomModel){
+    this.cartService.addRoom(room);
+    
+  }
+
+}
